746289a739b18e57fce1e0db0beef8b1af7fe1cb
zhangjie <157591509+lensenking@users.noreply.github.com>
2026-05-08 17:12:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,17 +4,12 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 1. 添加专门适配 24.10 现代 LuCI 界面的 Turbo ACC 源码
+git clone -b 18.06 https://github.com/chenmozhijin/turboacc.git package/turboacc
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-# 添加高性能 Turbo ACC 模块源码
-git clone -b luci https://github.com/chenmozhijin/turboacc.git package/turboacc
+# 2. 添加最新版 Nikki 官方源码（确保编译出最新版本）
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/luci-app-nikki
+
+# 3. 添加 MosDNS 官方源码
+git clone https://github.com/sbwml/luci-app-mosdns.git package/luci-app-mosdns
